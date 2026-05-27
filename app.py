200a42c16d205b14e89e0a181c75f97f830ea62b
Haripriyabs1 <bsharipriya1@gmail.com>
2026-05-28 00:00:08 +0530

    structure changes

--- app.py
+++ app.py
@@ -1,65 +1,18 @@
-import streamlit as st
-import PyPDF2
 import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+def preprocessing(text):
+    text=text.lower()
+    text=text.translate(str.maketrans("","",string.punctuation))
+    text=word_tokenize(text)
+    stop_words=set(stopwords.words("english"))
+    text=[word for word in text if word not in stop_words]
+    lemmatizer=WordNetLemmatizer()
+    text=[lemmatizer.lemmatize(word) for word in text]
+    text="".join(text)
+    return text
 
-st.title("Resume Keyword Finder")
-
-keyword = st.text_area("Enter The Required Skills (comma-separated)")
-resume = st.file_uploader("Upload The Resume", type=["pdf"])
-
-text = ""
-
-if resume is not None:
-
-    if keyword.strip() == "":
-        st.warning("Please enter required skills")
-
-    else:
-
-        keywordsplit = keyword.split(",")
-
-        keywordlower = [
-            key.strip().lower().translate(
-                str.maketrans('', '', string.punctuation)
-            )
-            for key in keywordsplit
-        ]
-
-        try:
-            reader = PyPDF2.PdfReader(resume)
-
-            for page in reader.pages:
-                text += page.extract_text()
-
-        except:
-            st.error("Error reading PDF file")
-
-        text = text.translate(
-            str.maketrans('', '', string.punctuation)
-        )
-
-        text = text.lower()
-
-        score = 0
-
-        st.subheader("Skill Match Results")
-
-        for keys in keywordlower:
-
-            if keys in text:
-                st.success(f"{keys} Matched Successfully")
-                score += 1
-
-            else:
-                st.error(f"{keys} Not Matched")
-
-        percentage = (score / len(keywordlower)) * 100
-
-        st.subheader("Final Resume Score")
-
-        st.metric(
-            "ATS Match Percentage",
-            f"{percentage:.2f}%"
-        )
-
-        st.progress(int(percentage))
