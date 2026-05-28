0d7126d1737547473cab382ccbf1368af2a83b36
Haripriyabs1 <bsharipriya1@gmail.com>
2026-05-29 00:20:38 +0530

    Added TF-IDF and cosine similarity for resume matching

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import string
+import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
@@ -16,6 +18,10 @@ def preprocessing(text):
     text=[lemmatizer.lemmatize(word) for word in text]
     text="".join(text)
     return text
-documents=['job_text','resume_text']
+job_text=preprocessing(job_text)
+resume_text=preprocessing(resume_text)
+documents=[job_text,resume_text]
 vectorizer=TfidfVectorizer()
 tfidf_matrix=vectorizer.fit_transform(documents)
+similarity_score=cosine_similarity(tfidf_matrix[0:1],tfidf_matrix[1:2])
+print("Similarity Score:",similarity_score[0][0])
